70bd011d70e94229a74397e93dc3bd24f5782cb5
Harsh Jain <harshjain5454@gmail.com>
2021-12-09 16:46:01 +0530

    Create Footer Component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className="footer">
+                <span className="made-by">Made with ❤️ by <a href="https://in.linkedin.com/in/harsh-jain-b9691b1b1" className="link" target="_blank" rel="noreferrer">Harsh</a></span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
