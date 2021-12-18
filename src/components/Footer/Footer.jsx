0359e375be81ac6ff0e749e0dad4eb6576b3c0cb
harshj11 <60258934+harshj11@users.noreply.github.com>
2021-12-18 17:04:29 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return(
         <footer>
             <div className="footer">
-                <span className="made-by">Made with ❤️ by <a href="https://in.linkedin.com/in/harsh-jain-b9691b1b1" className="link" target="_blank" rel="noreferrer">Harsh</a></span>
+                <span className="made-by">Made with ❤️ by <a href="https://twitter.com/harshj_11" className="link" target="_blank" rel="noreferrer">Harsh</a></span>
             </div>
         </footer>
     )
